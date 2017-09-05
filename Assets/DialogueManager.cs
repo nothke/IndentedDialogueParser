f9a5ddc9e92c7e98aa07dcb318ffbd61fd5f0ea2
Ivan Notaros <nothke@gmail.com>
2017-09-05 04:35:18 +0200

    Added a DialogueManager, as an example for automatic parsing and simple getting

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿///
+/// Combines all trees from a folder
+/// 
+/// Add this component into the scene, at it will parse all files at start
+/// 
+/// You can get any dialogue tree by simply querying by tree definition,
+/// eg. DialogueManager.Trees[DIALOGUE 1]
+/// 
+/// To start a dialogue, get DialogueManager.Trees[DIALOGUE 1].GetFirstNode();
+/// To get the next node, get DialogueManager.Trees[DIALOGUE 1].GetNext(*node*, *choiceNumber*);
+/// 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+using System.IO;
+using IndentedDialogue;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    void Awake() { instance = this; }
+
+    Dictionary<string, DialogueTree> treeDict = new Dictionary<string, DialogueTree>();
+
+    public static Dictionary<string, DialogueTree> Trees { get { return instance.treeDict; } }
+
+    public string folderName = "DialogueData";
+
+    public void Start()
+    {
+        string[] fileNames = Directory.GetFiles(folderName);
+
+        foreach (var fileName in fileNames)
+        {
+            var parsedDict = DialogueParser.ParseIntoTreeDict(fileName);
+            parsedDict.ToList().ForEach(x => treeDict.Add(x.Key, x.Value));
+        }
+    }
+}
